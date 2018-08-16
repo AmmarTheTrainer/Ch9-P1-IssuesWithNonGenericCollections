5ea8b035045467ff0dd96be8dfb0cbf91319453d
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-08-16 11:49:58 +0500

    first commit

---
+++ Ch9-P1-IssuesWithNonGenericCollections/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch9_P1_IssuesWithNonGenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Lecture 9 ");
+
+            Console.ReadLine();
+        }
+    }
+}
