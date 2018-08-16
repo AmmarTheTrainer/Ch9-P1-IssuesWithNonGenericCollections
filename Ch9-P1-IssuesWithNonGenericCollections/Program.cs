160a228b9a4d8127d86b7aa61aff8b28519b8ef8
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-08-16 17:04:40 +0500

    commit from bike repairing shop

--- Ch9-P1-IssuesWithNonGenericCollections/Program.cs
+++ Ch9-P1-IssuesWithNonGenericCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,88 @@ namespace Ch9_P1_IssuesWithNonGenericCollections
     {
         static void Main(string[] args)
         {
-            SimpleBoxUnboxOperation();
+            //SimpleBoxUnboxOperation();
+
+            //WorkWithArrayList();
+
+            //ArrayListOfRandomObjects();
+
+            //UsePersonCollection();
+
+            UseGenericList();
 
             Console.ReadLine();
+        }
+
+        private static void UseGenericList()
+        {
+            Console.WriteLine("***** Fun with Generics *****\n");
+            // This List<> can hold only Person objects.
+            List<Person> morePeople = new List<Person>();
+
+            morePeople.Add(new Person("Frank", "Black", 50));
+            Console.WriteLine(morePeople[0]);
+            
+            // This List<> can hold only integers.
+            List<int> moreInts = new List<int>();
+            moreInts.Add(10);
+            moreInts.Add(2);
+
+            int sum = moreInts[0] + moreInts[1];
+
+            // Compile-time error! Can't add Person object to a list of ints!
+            // moreInts.Add(new Person());
+
+        }
+
+        private static void UsePersonCollection()
+        {
+            Console.WriteLine("***** Custom Person Collection *****\n");
+
+            PersonCollection myPeople = new PersonCollection();
+
+            myPeople.AddPerson(new Person("Homer", "Simpson", 40));
+            myPeople.AddPerson(new Person("Marge", "Simpson", 38));
+            myPeople.AddPerson(new Person("Lisa", "Simpson", 9));
+            myPeople.AddPerson(new Person("Bart", "Simpson", 7));
+            myPeople.AddPerson(new Person("Maggie", "Simpson", 2));
+
+            // This would be a compile-time error! myPeople.AddPerson(new Car());
+            foreach (Person p in myPeople)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        private static void ArrayListOfRandomObjects()
+        {
+            // The ArrayList can hold anything at all.
+            ArrayList allMyObjects = new ArrayList();
+            allMyObjects.Add(true);
+            allMyObjects.Add(new OperatingSystem(PlatformID.MacOSX, new Version(10, 0)));
+            allMyObjects.Add(66);
+            allMyObjects.Add(3.14);
+
+        }
+
+        private static void WorkWithArrayList()
+        {
+            // Value types are automatically boxed when passed to a method requesting an object.
+            ArrayList myInts = new ArrayList();
+
+            //Console.WriteLine(myInts.GetType().Name);
+
+            myInts.Add(10);
+            myInts.Add(20);
+            myInts.Add(35);
+
+            //Console.WriteLine(myInts[1].GetType().Name);
+
+            // Unboxing occurs when an object is converted back to stack-based data.
+            int i = (int)myInts[0];
+
+            // Now it is reboxed, as WriteLine() requires object types!
+            Console.WriteLine("Value of your int: {0}", i);
         }
 
         private static void SimpleBoxUnboxOperation()
@@ -22,8 +102,20 @@ namespace Ch9_P1_IssuesWithNonGenericCollections
             // Box the int into an object reference.
             object boxedInt = myInt;
 
-            // Unbox the reference back into a corresponding int.
-            int unboxedInt = (int)boxedInt;
+            // Unbox in the wrong data type to trigger runtime exception.
+            try
+            {
+                long unboxedInt = (long)boxedInt;
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //ArrayList 
+
+            //// Unbox the reference back into a corresponding int.
+            //int unboxedInt = (int)boxedInt;
         }
     }
 }
